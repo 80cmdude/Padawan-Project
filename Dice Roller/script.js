68f0c415290998029f5cf467319c91a3dc1244a7
80cmdude <s80cmdude@gmail.com>
2016-04-14 11:47:30 +0100

    Multiple Results
    Added an option to view all results as seperate. Have broken the mod
    option will put that back in as its own thing

--- Dice Roller/script.js
+++ Dice Roller/script.js
@@ -3,18 +3,20 @@ function Roll() {
 	var userInput = document.getElementById("userInput").value;
 	var modifier = document.getElementById("modifier").value;
 	if (userInput == 0 || numDice == 0) {
-		var result = document.getElementById("result");
-		result.innerHTML = "I can't roll 0!";
+		var rolls = document.getElementById("rolls");
+		rolls.innerHTML = "I can't roll 0!";
 	}
 	else if (userInput == 1) {
-		var result = document.getElementById("result");
-		result.innerHTML = "I couldn't find any double headed coins. Sorry!";
+		var rolls = document.getElementById("rolls");
+		rolls.innerHTML = "I couldn't find any double headed coins. Sorry!";
 	}
 	else {
-		for (i = 0; i <= numDice; i++) {
-			var result = document.getElementById("result");
-			result.innerHTML = Math.floor(((Math.random() * userInput) + 1) + Math.floor(modifier)) * Math.floor(numDice);
-			i++;
+		var rolls = "";
+		var i;
+		for (i = 0; i < numDice; i++) {
+			rolls += Math.floor(((Math.random() * userInput) + 1)) + " ";
+			document.getElementById("rolls").innerHTML = rolls;
 		}
 	};
 }
+
