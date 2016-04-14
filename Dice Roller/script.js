6f200eaa504e5d115f64359b8689410e39013477
80cmdude <s80cmdude@gmail.com>
2016-04-14 12:48:15 +0100

    First Skeleton Complete!
    I have converted the for loop to print into an array then i display the
    array. I do this so i can use another for loop to add the elements
    together to form a total. Finally i add the mod to the full set for a
    full total!

--- Dice Roller/script.js
+++ Dice Roller/script.js
@@ -3,20 +3,27 @@ function Roll() {
 	var userInput = document.getElementById("userInput").value;
 	var modifier = document.getElementById("modifier").value;
 	if (userInput == 0 || numDice == 0) {
-		var rolls = document.getElementById("rolls");
-		rolls.innerHTML = "I can't roll 0!";
+		var array = document.getElementById("array");
+		array.innerHTML = "I can't roll 0!";
 	}
 	else if (userInput == 1) {
-		var rolls = document.getElementById("rolls");
-		rolls.innerHTML = "I couldn't find any double headed coins. Sorry!";
+		var array = document.getElementById("array");
+		array.innerHTML = "I couldn't find any double headed coins. Sorry!";
 	}
 	else {
-		var rolls = "";
-		var i;
+		var array = [];
 		for (i = 0; i < numDice; i++) {
-			rolls += Math.floor(((Math.random() * userInput) + 1)) + " ";
-			document.getElementById("rolls").innerHTML = rolls;
+		    var rolls = Math.floor(((Math.random() * userInput) + 1));
+    		array.push(rolls);
+    		document.getElementById("array").innerHTML = array;
 		}
 	};
+	var total = 0;
+	for (i = 0; i < array.length; i++) {
+    	total += array[i];
+	}
+	document.getElementById("total").innerHTML = total;
+	var totalMod = Math.floor(total) + Math.floor(modifier);
+	document.getElementById("totalMod").innerHTML = totalMod;
 }
 
