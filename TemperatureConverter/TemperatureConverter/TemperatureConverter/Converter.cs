8e8d27c3b132ec78fbba16149f045097b72d635d
Samuel Rule <samuel.rule@reedonline.co.uk>
2016-10-19 11:29:15 +0100

    Added new app for temp converter

---
+++ TemperatureConverter/TemperatureConverter/TemperatureConverter/Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    public class Converter
+    {
+
+        public bool GetUserInput(string userinput)
+        {
+            try
+            {
+                Convert.ToInt32(userinput);
+                return true;
+            }
+            catch (FormatException e)
+            {
+                return false;
+            }
+        }
+        public string ConvertToCelcius(int userTemperature)
+        {
+            int celciusValue = (userTemperature - 32) * 5 / 9;
+            return celciusValue.ToString();
+        }
+    }
+}
---
+++ TemperatureConverter/TemperatureConverter/TemperatureConverter/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TemperatureConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ConvertTemperature(object sender, EventArgs e)
+        {
+            Converter ConvertTemp = new Converter();
+
+            bool convertedSuccessfully = ConvertTemp.GetUserInput(txtTemperatureEntry.Text);
+
+            if (convertedSuccessfully)
+            {
+                int userTemperature = Convert.ToInt32(txtTemperatureEntry.Text);
+                string convertedTemperature = ConvertTemp.ConvertToCelcius(userTemperature);
+                txtConvertedTemperature.Text = convertedTemperature + " C";
+            }
+            else
+            {
+                txtConvertedTemperature.Text = "Please enter a number";
+            }
+        }
+    }
+}
