a336131a7649da2cad53cad9075c4c8659409813
80cmdude <s80cmdude@gmail.com>
2016-11-04 17:15:36 +0000

    It all works Awesomes

--- SamsUsefulApp/SamsUsefulApp/SamsUsefulApp/Views/DiceRollPage.xaml.cs
+++ SamsUsefulApp/SamsUsefulApp/SamsUsefulApp/Views/DiceRollPage.xaml.cs
@@ -21,10 +21,26 @@ namespace SamsUsefulApp.Views
             int numDice = Convert.ToInt32(numberOfDice.Text);
             int numSides = Convert.ToInt32(numberOfSides.Text);
 
-            DiceRoll Roll = new DiceRoll();
-            string[] resultArray = Roll.RollTheDice(numDice, numSides);
-            string result = string.Join(", ", resultArray);
-            txtRolledDice.Text = result;
+            if (numDice > 100 || numSides > 100)
+            {
+                txtRolledDice.Text = "You cannot roll more than 100 dice or a dice with over 100 sides";
+            }
+
+            else
+            {
+                DiceRoll Roll = new DiceRoll();
+                string[] resultArray = Roll.RollTheDice(numDice, numSides);
+                string result = string.Join(", ", resultArray);
+                txtRolledDice.Text = result;
+
+                List<int> totalArray = new List<int>();
+                foreach (var i in resultArray)
+                {
+                    totalArray.Add(Convert.ToInt16(i));
+                }
+
+                total.Text = totalArray.Sum().ToString();
+            }
         }
     }
 }
