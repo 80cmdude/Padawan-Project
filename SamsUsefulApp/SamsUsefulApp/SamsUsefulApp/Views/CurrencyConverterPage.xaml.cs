a81d5c323b23e7c81652ad2b2eea6d8c523c2ed9
80cmdude <s80cmdude@gmail.com>
2016-11-04 10:36:45 +0000

    Currency Converted Works!!

--- SamsUsefulApp/SamsUsefulApp/SamsUsefulApp/Views/CurrencyConverterPage.xaml.cs
+++ SamsUsefulApp/SamsUsefulApp/SamsUsefulApp/Views/CurrencyConverterPage.xaml.cs
@@ -16,12 +16,28 @@ namespace SamsUsefulApp.Views
         public CurrencyConverterPage()
         {
             InitializeComponent();
-        }
 
-        private void CurrencyPicker()
-        {
             var currencies = Queries.GetItems();
             Dictionary<string, decimal> result = currencies.ToDictionary(x => x.Currency, x => x.ConversionRate);
+
+            Label currencyConverter = new Label
+            {
+                Text = "Currency Converter"
+            };
+            Label ExchangeRate = new Label
+            {
+                Text = ""
+            };
+
+            Label convertedValue = new Label
+            {
+                Text = ""
+            };
+
+            Entry GBP = new Entry
+            {
+                Keyboard = Keyboard.Numeric
+            };
 
             Picker picker = new Picker
             {
@@ -32,6 +48,28 @@ namespace SamsUsefulApp.Views
             {
                 picker.Items.Add(currencyValues);
             }
+
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    currencyConverter,
+                    GBP,
+                    picker,
+                    ExchangeRate,
+                    convertedValue
+                }
+            };
+
+            picker.SelectedIndexChanged += (sender, args) =>
+            {
+                string currencyValue = picker.Items[picker.SelectedIndex];
+                string exchangeRate = result[currencyValue].ToString();
+                ExchangeRate.Text = exchangeRate;
+
+                string finalConvertedValue = (Convert.ToDecimal(GBP.Text) * result[currencyValue]).ToString();
+                convertedValue.Text = finalConvertedValue;
+            };
         }
     }
     
