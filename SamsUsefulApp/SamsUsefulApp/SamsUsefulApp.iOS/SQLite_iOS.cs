a12a47c3dfeb54a7c1880c5e02becc0b2458cb77
80cmdude <s80cmdude@gmail.com>
2016-11-15 14:48:10 +0000

    Hopefully works for ios

---
+++ SamsUsefulApp/SamsUsefulApp/SamsUsefulApp.iOS/SQLite_iOS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SQLite.Net;
+using SamsUsefulApp;
+using SamsUsefulApp.iOS;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+namespace SamsUsefulApp.iOS
+{
+    public class SQLite_iOS : ISQLite
+    {
+        SQLiteConnection ISQLite.GetConnection()
+        {
+            var sqliteFilename = "TodoSQLite.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            string libraryPath = Path.Combine(documentsPath, "..", "Library"); // Library folder
+            var path = Path.Combine(libraryPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLiteConnection(new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS(), path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
